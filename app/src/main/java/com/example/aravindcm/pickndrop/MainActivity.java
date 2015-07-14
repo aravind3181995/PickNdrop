ca70bbcb8559f581306f724ae2da23f59d40c2a6
aravind3181995 <aravind3181995@gmail.com>
2015-07-14 19:42:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aravindcm/pickndrop/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.aravindcm.pickndrop;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
+public class MainActivity extends Activity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.e("cm", rawResult.getText()); // Prints scan results
+        Toast.makeText(getApplicationContext(), rawResult.getText(),
+                Toast.LENGTH_LONG).show();
+        Log.e("cm", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+    }
+}
